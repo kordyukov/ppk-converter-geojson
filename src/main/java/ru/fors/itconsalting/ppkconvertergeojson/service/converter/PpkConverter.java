4f9f67294fed76301c80aae1a433403a53ec61c0
denis.kordyukov <denis.kordyukov@fors.ru>
2024-04-14 12:48:18 +0300

    @

---
+++ src/main/java/ru/fors/itconsalting/ppkconvertergeojson/service/converter/PpkConverter.java
@@ -0,0 +1,12 @@
+package ru.fors.itconsalting.ppkconvertergeojson.service.converter;
+
+//TODO нужна формула преобразования координат MSK-01 в EPSG::3857
+public class PpkConverter {
+    public static double convertX(double x) {
+        return 0;
+    }
+
+    public static double convertY(double y) {
+        return 0;
+    }
+}
